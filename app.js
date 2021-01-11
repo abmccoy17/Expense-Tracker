c193bdd2ef9513b44634165052287ccbdb287627
Alex McCoy <abmccoy17@gmail.com>
2021-01-11 17:38:29 -0500

    Added bulma styling and more logic for total expense

--- app.js
+++ app.js
@@ -4,35 +4,50 @@ const expenseAmount = document.querySelector('#expenseAmount');
 const tableBody = document.querySelector('#tableBody');
 const addExpenseBtn = document.querySelector('#addExpenseBtn');
 const exampleRow = document.querySelector('#exampleRow');
+const deleteBtn = document.createElement("button");
 
 addExpenseBtn.addEventListener('click', myFunction);
 
-function myFunction() {
-    const row = tableBody.insertRow(1);
-    const cell1 = row.insertCell(0);
-    const cell2 = row.insertCell(1);
-    const cell3 = row.insertCell(2);
-    const cell4 = row.insertCell(3);
-    
-    const deleteBtn = document.createElement('button');
-    deleteBtn.setAttribute('id', 'deleteBtn');
-    deleteBtn.innerText = 'x';
-    deleteBtn.addEventListener('click', function() {
-        row.remove();
-    })
+let expenseTotal = 0;
+let expenseTotalDisplay = document.querySelector("#expenseTotal");
 
-    cell1.innerHTML = `${expenseName.value}`;
-    cell2.innerHTML = `${expenseDate.value}`;
-    cell3.innerHTML = `$${expenseAmount.value}`;
-    cell4.append(deleteBtn);
+function myFunction() {
+    
+    const row = tableBody.insertRow(1);
+
+    deleteBtn.setAttribute("id", "deleteBtn");
+    deleteBtn.setAttribute("class", "button is-danger");
+    deleteBtn.innerText = "x";
+
+    if (expenseName.value === '') {
+        alert('Please fill out the name of the expense');
+    } else if (expenseDate.value === '') {
+        alert('Please enter the date of the expense');
+    } else if (expenseAmount.value === '') {
+        alert('Please enter the dollar amount of the expense');
+    } else {
+        const cell1 = row.insertCell(0);
+        const cell2 = row.insertCell(1);
+        const cell3 = row.insertCell(2);
+        const cell4 = row.insertCell(3);
+        cell1.innerHTML = `${expenseName.value}`;
+        cell2.innerHTML = `${expenseDate.value}`;
+        cell3.innerHTML = `$${expenseAmount.value}`;
+        cell4.append(deleteBtn);
+    };
+
+    deleteBtn.addEventListener("click", function () {
+        row.remove();
+        expenseTotal -= parseInt(expenseAmount.value);
+    });
+
+    expenseTotal += parseInt(expenseAmount.value);
+    expenseTotalDisplay.innerText = expenseTotal;
 
     expenseName.value = '';
     expenseDate.value = '';
     expenseAmount.value = '';
-
-    exampleRow.remove();
-}
-
+};
 
  
 
